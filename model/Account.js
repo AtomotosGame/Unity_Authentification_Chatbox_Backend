f070398491ecea5a95dad13dc70395517deac11c
Backendeng <backendeng0417@gmail.com>
2022-11-20 18:09:17 -0500

    Add comment

--- model/Account.js
+++ model/Account.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const accountSchema = new Schema({
-email: String,
-password: String,
-planername: String,
-playerlevel: Number,
-level: Number,
-gold: Number,
-elixir: Number,
-blackelixir: Number,
-gem: Number,
-maxgold: Number,
-maxlixir: Number,
-ownedbuildings: [],
-lastAuthentication: Date,
+    email: String,
+    password: String,
+    planername: String,
+    playerlevel: Number,
+    level: Number,
+    gold: Number,
+    elixir: Number,
+    blackelixir: Number,
+    gem: Number,
+    maxgold: Number,
+    maxlixir: Number,
+    ownedbuildings: [],
+    lastAuthentication: Date,
 });
 
 mongoose.model('accounts', accountSchema);
